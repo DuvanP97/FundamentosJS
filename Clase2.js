bd4073a7f913187e25d212a2e9da014549185df9
Duvan <duvan.a.pedroza.n@gmail.com>
2019-05-26 17:24:35 -0500

    Update Clase2.js

---
+++ Clase2.js
@@ -0,0 +1,11 @@
+var nombre = 'Duvan', apellido = 'Pedroza'
+
+var nombreEnMayusculas= nombre.toUpperCase()
+var apellidoEnMinusculas= apellido.toLowerCase()
+
+var primeraLetra = nombre.charAt(0)
+var cantidadLetras = nombre.length
+
+var nombreCompleto = `${nombre} ${apellido.toUpperCase}`
+
+var str = nombre.substr(1, 2)
